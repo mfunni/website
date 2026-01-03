783adab839c4f0a0c0fb9b6c1edfb99572ea3f96
mfunni <Joerngaedecke@gmail.com>
2026-01-03 21:23:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("tile-1").addEventListener("click", function () {
-  document.getElementById("panel").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+});
+
+document.getElementById("close").addEventListener("click", function () {
+  document.getElementById("overlay").style.display = "none";
 });
