ae8e0f28b8f2eff09ce68ab12dc604f72b2e004a
mfunni <Joerngaedecke@gmail.com>
2026-01-03 23:00:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ document.getElementById("tile-1").addEventListener("click", function () {
 document.getElementById("close").addEventListener("click", function () {
   document.getElementById("overlay").style.display = "none";
 });
+document.getElementById("tile-8").addEventListener("click", function () {
+  document.getElementById("overlay-title").textContent = "Ragna ist cool!";
+  document.getElementById("overlay").style.display = "block";
+});
