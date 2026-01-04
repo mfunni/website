859625dc05927c1f9245b8c3e4501bab77153e5e
mfunni <Joerngaedecke@gmail.com>
2026-01-04 08:37:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("tile-1").addEventListener("click", function () {
+  document.getElementById("overlay-title").textContent = "Trinkspiel";
   document.getElementById("overlay").style.display = "block";
 });
-
 document.getElementById("close").addEventListener("click", function () {
   document.getElementById("overlay").style.display = "none";
 });
