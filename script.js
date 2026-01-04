d3ad9f100e4b876b4e729aa6b94ab5a5e473bb53
mfunni <Joerngaedecke@gmail.com>
2026-01-04 08:52:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById("tile-1").addEventListener("click", function () {
   document.getElementById("overlay-title").textContent = "Trinkspiel";
+  document.getElementById("overlay-text").textContent =
+    "Hier kommt gleich die Eingabe der Spieleranzahl.";
   document.getElementById("overlay").style.display = "block";
 });
 document.getElementById("close").addEventListener("click", function () {
@@ -7,5 +9,6 @@ document.getElementById("close").addEventListener("click", function () {
 });
 document.getElementById("tile-8").addEventListener("click", function () {
   document.getElementById("overlay-title").textContent = "Ragna ist cool!";
+  document.getElementById("overlay-text").textContent = "";
   document.getElementById("overlay").style.display = "block";
 });
